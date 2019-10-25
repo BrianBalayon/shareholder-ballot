5bf36fa9bb9c01065c4e96cfbf422288a59cc055
Brian Balayon <brianbalayon@gmail.com>
2019-10-25 15:48:51 -0400

    Set up middle layer to some extent (I think). Need to specify function handlers still

--- ballot-app/src/index.js
+++ ballot-app/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var express = require('express');
+var app = express();
+app.use(express.static('src'));
+app.use(express.static('../ballot-contract/build/contracts'));
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
